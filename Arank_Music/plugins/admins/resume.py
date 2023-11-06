a5c33bee3a67240e935bbffde98626fae7223857
CoderXKrishna <144244911+CoderXKrishna@users.noreply.github.com>
2023-11-06 05:51:54 -0800

    Update resume.py

--- Arank_Music/plugins/admins/resume.py
+++ Arank_Music/plugins/admins/resume.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.utils.database import is_music_playing, music_on
-from AnonXMusic.utils.decorators import AdminRightsCheck
-from AnonXMusic.utils.inline import close_markup
+from Arank_Music import app
+from Arank_Music.core.call import Anony
+from Arank_Music.utils.database import is_music_playing, music_on
+from Arank_Music.utils.decorators import AdminRightsCheck
+from Arank_Music.utils.inline import close_markup
 from config import BANNED_USERS
 
 
