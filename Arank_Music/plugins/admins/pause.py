79a2e12632d39051a43f438f6f24c6a7eb3f9f8d
CoderXKrishna <144244911+CoderXKrishna@users.noreply.github.com>
2023-11-06 18:34:42 +0530

    Update pause.py

--- Arank_Music/plugins/admins/pause.py
+++ Arank_Music/plugins/admins/pause.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from DanishXmusic21 import app
-from DanishXmusic21.core.call import Anony
-from DanishXmusic21.utils.database import is_music_playing, music_off
-from DanishXmusic21.utils.decorators import AdminRightsCheck
-from DanishXmusic21.utils.inline import close_markup
+from Arank_Music import app
+from Arank_music.core.call import Arank
+from Arank_Music.utils.database import is_music_playing, music_off
+from Arank_Music.utils.decorators import AdminRightsCheck
+from Arank_Music.utils.inline import close_markup
 from config import BANNED_USERS
 
 
@@ -15,7 +15,7 @@ async def pause_admin(cli, message: Message, _, chat_id):
     if not await is_music_playing(chat_id):
         return await message.reply_text(_["admin_1"])
     await music_off(chat_id)
-    await Anony.pause_stream(chat_id)
+    await Arank.pause_stream(chat_id)
     await message.reply_text(
         _["admin_2"].format(message.from_user.mention), reply_markup=close_markup(_)
     )
