73165e85ff2ca6a000034dc4307e67fde62bd7f2
CoderXKrishna <144244911+CoderXKrishna@users.noreply.github.com>
2023-11-06 06:00:26 -0800

    Update auth.py

--- Arank_Music/plugins/admins/auth.py
+++ Arank_Music/plugins/admins/auth.py
@@ -1,16 +1,16 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.utils import extract_user, int_to_alpha
-from AnonXMusic.utils.database import (
+from Arank_Music import app
+from Arank_Music.utils import extract_user, int_to_alpha
+from Arank_Music.utils.database import (
     delete_authuser,
     get_authuser,
     get_authuser_names,
     save_authuser,
 )
-from AnonXMusic.utils.decorators import AdminActual, language
-from AnonXMusic.utils.inline import close_markup
+from Arank_Music.utils.decorators import AdminActual, language
+from Arank_Music.utils.inline import close_markup
 from config import BANNED_USERS, adminlist
 
 
