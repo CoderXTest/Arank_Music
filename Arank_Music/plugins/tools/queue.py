79ee9f908f61dbb5c3eebd6b531b327fc17e229e
CoderXKrishna <144244911+CoderXKrishna@users.noreply.github.com>
2023-11-06 18:38:28 +0530

    Update queue.py

--- Arank_Music/plugins/tools/queue.py
+++ Arank_Music/plugins/tools/queue.py
@@ -8,7 +8,7 @@ from pyrogram.types import CallbackQuery, InputMediaPhoto, Message
 import config
 from Arank_Music import app
 from Arank_Music.misc import db
-from Arank_Music.utils import AnonyBin, get_channeplayCB, seconds_to_min
+from Arank_Music.utils import ArankBin, get_channeplayCB, seconds_to_min
 from Arank_Music.utils.database import get_cmode, is_active_chat, is_music_playing
 from Arank_Music.utils.decorators.language import language, languageCB
 from Arank_Music.utils.inline import queue_back_markup, queue_markup
@@ -176,7 +176,7 @@ async def queued_tracks(client, CallbackQuery: CallbackQuery, _):
             return await CallbackQuery.edit_message_text(msg, reply_markup=buttons)
         if "✨" in msg:
             msg = msg.replace("✨", "")
-        link = await AnonyBin(msg)
+        link = await ArankBin(msg)
         med = InputMediaPhoto(media=link, caption=_["queue_3"].format(link))
         await CallbackQuery.edit_message_media(media=med, reply_markup=buttons)
     else:
