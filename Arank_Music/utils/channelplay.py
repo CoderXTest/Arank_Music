aebd9e75e7443bc05b9efb6eaa31c8d52ebbb378
CoderXKrishna <144244911+CoderXKrishna@users.noreply.github.com>
2023-11-06 05:59:36 -0800

    Update channelplay.py

--- Arank_Music/utils/channelplay.py
+++ Arank_Music/utils/channelplay.py
@@ -1,5 +1,5 @@
-from AnonXMusic import app
-from AnonXMusic.utils.database import get_cmode
+from Arank_Music import app
+from Arank_Music.utils.database import get_cmode
 
 
 async def get_channeplayCB(_, command, CallbackQuery):
